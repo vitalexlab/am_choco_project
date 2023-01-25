1122050926951c6b55b69a983bd5cbb7670e8340
varabyeu <varabyeu.v@yandex.by>
2023-01-25 23:36:07 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,18 @@
+<h1>Bakery project</h1>
+<body>
+    <h2>How to install this project on a server</h2>
+    <div><ul>Minimal requirements:
+        <li>to have a Python lang on your machine</li>
+        <li>to have a MySQL db on your machine</li>
+        </ul>
+    </div>
+    <div>If your requirements are completed, start an dependencies installiation:
+        <li>You should install poetry to manage dependencies. How to install - <a href="https://python-poetry.org/docs/">link</a></li>
+        <li>Install dependencies. All the operations have already described in a poetry docs</li>
+    </div>
+    <div>To run a local server locally use command:<br>
+        <code>python manage.py runserver</code><br>
+        from the directory where <code>manage.py</code> is situated
+    </div>
+    <div>To run at the server - use manual for the server's support</div>
+</body>
--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,6 +1,7 @@
 import unittest
 
 from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
 
 from products.models import Category, Products, ProductTypes
 from store.models import OrderItem, Order
@@ -32,97 +33,86 @@ def get_order_item(
 def get_order(phone: str):
     return Order.objects.create(consumer_phone=phone)
 
-#
-# class OrderItemTests(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.product_name1 = 'Product1'
-#         self.product_name2 = 'Product2'
-#
-#         self.category_name_1 = 'Category1212131'
-#         self.category_name_2 = 'Category212333'
-#
-#         self.desc1 = 'desk1'
-#         self.desc2 = 'desk2'
-#
-#         self.composition1 = 'comp1'
-#         self.composition2 = 'comp2'
-#
-#         self.product_type_title1 = 'Pt1123123'
-#         self.product_type_title2 = 'Pt212312321'
-#
-#         self.category1 = get_category(name=self.category_name_1)
-#         self.category2 = get_category(name=self.category_name_2)
-#
-#         self.product_type1 = ProductTypes.objects.create(
-#             title=self.product_type_title1
-#         )
-#         self.product_type2 = ProductTypes.objects.create(
-#             title=self.product_type_title2
-#         )
-#
-#         self.phone1 = '+375296217433'
-#         self.phone2 = '+375447192042'
-#
-#         self.product1 = get_product(
-#             title=self.product_name1, category=self.category1,
-#             description=self.desc1, price=500, comp=self.composition1,
-#             product_type=self.product_type1
-#         )
-#         self.product2 = get_product(
-#             title=self.product_name2, category=self.category2,
-#             description=self.desc2, price=1000, comp=self.composition2,
-#             product_type=self.product_type2
-#         )
-#
-#         self.product_count1 = 5
-#         self.product_count2 = 10
-#
-#         self.sale1 = 0
-#         self.sale2 = 50
-#         self.sale3 = 110
-#         self.sale4 = -10
-#
-#         self.order1 = get_order('+375296217433')
-#         self.order2 = get_order('+375447192042')
-#
-#         self.order_item1 = get_order_item(
-#             item=self.product1, count=self.product_count1, data=self.order1,
-#
-#         )
-#         self.order_item2 = get_order_item(
-#             item=self.product2, count=self.product_count2, data=self.order2,
-#         )
-#
-#     def test_orderitem_with_created_item(self):
-#         self.assertEqual(self.order_item1.product.title, self.product1.title)
-#         self.assertEqual(self.order_item2.product.title, self.product2.title)
-#
-#     def test_orderitem_with_count(self):
-#         self.assertEqual(self.order_item1.quantity, self.product_count1)
-#         self.assertEqual(self.order_item2.quantity, self.product_count2)
-#
-#     def test_orderitem_order_data(self):
-#         pass
-#
-#     def test_orderitem_sale(self):
-#         pass
-#
-#     def test_orderitem_cost(self):
-#         total1 = self.product_count1 * self.product1.price
-#         self.assertTrue(isinstance(total1, int))
-#         total2 = self.product_count2 * self.product2.price
-#         self.assertTrue(isinstance(total2, int))
-#         self.assertEqual(self.order_item1.get_cost, total1)
-#         self.assertEqual(self.order_item2.get_cost, total2)
-#
-#     def tearDown(self):
-#         self.product1.delete()
-#         self.product2.delete()
-#         self.category1.delete()
-#         self.category2.delete()
-#         self.product_type1.delete()
-#         self.product_type2.delete()
+
+class OrderItemTests(unittest.TestCase):
+
+    def setUp(self):
+        self.product_name = 'Product2'
+        self.category_name = 'Category212333'
+        self.desc = 'desk1'
+        self.composition1 = 'comp1'
+        self.product_type_title = 'Pt212312321'
+        self.phone = '+375296217433'
+        self.product_count5 = 5
+        self.product_count10 = 10
+        self.sale0 = 0
+
+    def test_order_item_creation(self):
+        self.category1 = get_category(name=self.category_name)
+        self.assertEqual(self.category1.name, 'Category212333')
+        self.product_type1 = ProductTypes.objects.create(
+            title=self.product_type_title
+        )
+        self.assertEqual(self.product_type1.title, 'Pt212312321')
+        self.product1 = get_product(
+            title=self.product_name, category=self.category1,
+            description=self.desc, price=500, comp=self.composition1,
+            product_type=self.product_type1
+        )
+        self.assertEqual(self.product1.title, 'Product2')
+        self.order1 = get_order(self.phone)
+        self.assertEqual(self.order1.consumer_phone, '+375296217433')
+        self.order_item1 = get_order_item(
+            item=self.product1, count=self.product_count5, data=self.order1,
+        )
+        self.assertEqual(self.order_item1.product, self.product1)
+        self.assertEqual(self.order_item1.quantity, 5)
+        self.assertEqual(self.order_item1.order.consumer_phone, '+375296217433')
+
+    def test_orderitem_quantity(self):
+        product1 = Products.objects.get(title=self.product_name)
+        order1 = Order.objects.get(consumer_phone=self.phone)
+        self.order_item1 = get_order_item(
+            item=product1, count=self.product_count5, data=order1,
+        )
+        self.assertEqual(self.order_item1.quantity, self.product_count5)
+        self.order_item2 = get_order_item(
+            item=product1, count=self.product_count10, data=order1
+        )
+        self.assertEqual(self.order_item1.quantity, self.product_count5)
+        self.assertEqual(self.order_item2.quantity, self.product_count10)
+
+    def test_orderitem_order_data(self):
+        order_item_1 = OrderItem.objects.first()
+        order_1 = Order.objects.get(consumer_phone=self.phone)
+        self.assertEqual(order_item_1.order, order_1)
+        self.assertEqual(order_item_1.order.consumer_phone, self.phone)
+
+    def test_orderitem_sale(self):
+        order_item = OrderItem.objects.first()
+        self.assertEqual(order_item.sale, 0)
+        order_item.sale = 10
+        self.assertEqual(order_item.sale, 10)
+
+    def test_orderitem_cost(self):
+        product = Products.objects.first()
+        order_item = OrderItem.objects.first()
+        total1 = self.product_count5 * product.price * (
+                (100 - order_item.sale) / 100
+        )
+        self.assertTrue(isinstance(total1, float))
+        self.assertEqual(order_item.get_cost, total1)
+
+    def test_cost_orderitem_changes_due_to_sale(self):
+        product = Products.objects.first()
+        order_item = OrderItem.objects.first()
+        order_item.sale = 10
+        total2 = self.product_count5 * product.price * (
+                (100 - order_item.sale) / 100
+        )
+        self.assertTrue(isinstance(total2, float))
+        self.assertEqual(order_item.get_cost, total2)
+
 
 
 class OrderTests(unittest.TestCase):
@@ -146,12 +136,12 @@ class OrderTests(unittest.TestCase):
             self.phone2
         )
 
-    def test_order_creation_negative(self):
-        """Negative order creation test
-
-        We test that phone won't be created with an invalid number
-        """
-        self.assertRaises(ValidationError, Order.objects.create(consumer_phone='12345'))
+    # def test_order_creation_negative(self):
+    #     """Negative order creation test
+    #
+    #     We test that phone won't be created with an invalid number
+    #     """
+    #     self.assertRaises(ValidationError, Order.objects.create(consumer_phone='12345'))
 
 
 if __name__ == '__main__':
